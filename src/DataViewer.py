5962d8e289c682be87b5b561efc0def48a4296f3
dmille56 <donovanm56@gmail.com>
2017-06-27 23:16:30 -0600

    updating DataViewer.py adding listbox + scrolling

--- src/DataViewer.py
+++ src/DataViewer.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-import os
+import os, pickle
 
 samples_dir = 'samples'
 
@@ -10,21 +10,56 @@ class DataViewerApp(object):
     self.root = tk.Tk()
     self.root.wm_title("SpartanMarioKartAi - View Training Data")
 
-    self.tree = ttk.Treeview(self.root)
+    self.treeFrame = tk.Frame(self.root)
+    self.treeYScroll = tk.Scrollbar(self.treeFrame, orient=tk.VERTICAL)
+    self.treeYScroll.pack(side=tk.RIGHT, fill=tk.Y)
+    self.tree = ttk.Treeview(self.treeFrame, yscrollcommand=self.treeYScroll.set)
+    self.tree["columns"] = ("one")
+    self.tree.column("one", width=100)
+    self.tree.heading("one", text="is directory?")
+    self.tree.bind("<<TreeviewSelect>>", self.loadListBox)
+    self.treeYScroll.config(command=self.tree.yview)
     self.loadTree(samples_dir)
     self.tree.pack()
+    self.treeFrame.pack()
+
+    self.listboxFrame = tk.Frame(self.root)
+    self.listboxYScroll = tk.Scrollbar(self.listboxFrame, orient=tk.VERTICAL)
+    self.listboxYScroll.pack(side=tk.RIGHT, fill=tk.Y)
+    self.listbox = tk.Listbox(self.listboxFrame, yscrollcommand=self.listboxYScroll.set)
+    self.listboxYScroll.config(command=self.listbox.yview)
+    self.listbox.pack()
+    self.listboxFrame.pack()
 
   #load the file system into the treeview
   def loadTree(self, rootDir):
       self.tree.delete(*self.tree.get_children())
-      self.tree.insert('', 'end', rootDir, text=rootDir)
+      self.tree.insert('', tk.END, rootDir, text=rootDir, values=("yes"))
       self.loadTreeRecursive(rootDir)
 
   def loadTreeRecursive(self, rootDir):
       for f in os.listdir(rootDir):
-          self.tree.insert(rootDir, 'end', rootDir + '/' + f, text=f)
-          if(os.path.isdir(rootDir + '/' + f)):
-              self.loadTreeRecursive(rootDir + '/' + f)
+          fullName = rootDir + '/' + f
+          isDir = os.path.isdir(fullName)
+          if(isDir):
+              dirValue = "yes"
+          else:
+              dirValue = "no"
+          self.tree.insert(rootDir, tk.END, fullName, text=f, values=(dirValue))
+          if(isDir):
+              self.loadTreeRecursive(fullName)
+
+  def loadListBox(self, event):
+      self.listbox.delete(0, tk.END)
+      selected_item = self.tree.focus()
+      try:
+          if(not os.path.isdir(selected_item)):
+            with open(selected_item, "rb") as input_file:
+                self.sampleData = pickle.load(input_file)
+                for i in range(len(self.sampleData)):
+                    self.listbox.insert(tk.END, str(i))
+      except:
+          print('error loading selected file: ' + selected_item)
 
 dataViewer = DataViewerApp()
 dataViewer.root.mainloop()
