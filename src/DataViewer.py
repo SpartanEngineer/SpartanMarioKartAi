bfce3001121ecd12181a0245736521dd2f3a9403
dmille56 <donovanm56@gmail.com>
2017-06-28 23:19:32 -0600

    adding frame to dataviewer

--- src/DataViewer.py
+++ src/DataViewer.py
@@ -5,11 +5,10 @@ import os, pickle
 
 samples_dir = 'samples'
 
-class DataViewerApp(object):
+class DataViewerFrame(object):
 
-  def __init__(self):
-    self.root = tk.Tk()
-    self.root.wm_title("SpartanMarioKartAi - View Training Data")
+  def __init__(self, master):
+    self.root = tk.Frame(master)
 
     self.treeFrame = tk.Frame(self.root)
     self.treeYScroll = tk.Scrollbar(self.treeFrame, orient=tk.VERTICAL)
@@ -36,6 +35,8 @@ class DataViewerApp(object):
     self.ssLabel = tk.Label(self.root)
     self.ssLabel.pack()
 
+    self.root.pack()
+
   #load the file system into the treeview
   def loadTree(self, rootDir):
       self.tree.delete(*self.tree.get_children())
@@ -61,10 +62,14 @@ class DataViewerApp(object):
           if(not os.path.isdir(selected_item)):
             with open(selected_item, "rb") as input_file:
                 self.sampleData = pickle.load(input_file)
+                self.sampleDataDict = {}
                 for i in range(len(self.sampleData)):
                     self.listbox.insert(tk.END, str(i))
-      except:
-          print('error loading selected file: ' + selected_item)
+                    self.sampleDataDict[str(i)] = self.sampleData[i]
+
+      except Exception as e:
+        print('error loading selected file: ' + selected_item)
+        print(str(e))
 
   def loadScreenshotIntoLabel(self, event):
       selected_item = self.listbox.curselection()
@@ -73,5 +78,7 @@ class DataViewerApp(object):
       self.ssLabel.configure(image=self.ss)
       self.ssLabel.update_idletasks()
 
-dataViewer = DataViewerApp()
-dataViewer.root.mainloop()
+root = tk.Tk()
+root.wm_title("SpartanMarioKartAi - View Training Data")
+dataViewer = DataViewerFrame(root)
+root.mainloop()
